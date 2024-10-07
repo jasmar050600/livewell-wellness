6b3ac2ff3cbd819079de8439fbf7d4b94e6d6d8e
jasmar050600 <jasmar91@gmail.com>
2024-10-07 18:23:12 +0800

    6 23pm

--- Livewell/server.js
+++ Livewell/server.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const path = require('path')
-const app = express()
+const express = require('express');
+const path = require('path');
+const app = express();
 
-app.use(express.static(path.join(__dirname, '/dist')))
+app.use(express.static(path.join(__dirname, '/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/index.html'))
-})
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
 
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+const port = process.env.PORT || 3000;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${port}`);
+});
